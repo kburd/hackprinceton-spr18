4b2a2d59777b825791c4d02f5e266257c9ccda59
kburd <kburd@udel.edu>
2018-03-31 18:04:05 +0000

    minor mod on Indexer_v2 and finished turning classifier into a class

--- Classifier.py
+++ Classifier.py
@@ -6,11 +6,12 @@ class Classifier:
     
     def __init__(self):
         
-        print('')
+        self.class_words = {}
+        self.stemmer = LancasterStemmer()
+        self.corpus_words = {}
         
     def train(self):
         
-        stemmer = LancasterStemmer()
         training_data = []
 
         # read training data
@@ -24,7 +25,7 @@ class Classifier:
             subFolders.remove('.DS_Store')
 
         for sub in subFolders:
-            
+            print(sub)
             subfolder = os.listdir(master + "/" + sub)
             
             if '.DS_Store' in subfolder:
@@ -36,13 +37,11 @@ class Classifier:
                 for next in content:
                     training_data.append({"class":sub, "sentence":next})
                     
-        corpus_words = {}
-        class_words = {}
         # turn a list into a set (of unique items) and then a list again (this removes duplicates)
         classes = list(set([a['class'] for a in training_data]))
         for c in classes:
             # prepare a list of words within each class
-            class_words[c] = []
+            self.class_words[c] = []
 
         # loop through each sentence in our training data
         for data in training_data:
@@ -51,56 +50,50 @@ class Classifier:
                 # ignore a some things
                 if  word not in ["?", "'s"]:
                     # stem and lowercase each word
-                    stemmed_word = stemmer.stem(word.lower())
+                    stemmed_word = self.stemmer.stem(word.lower())
                     # have we not seen this word already?
-                    if stemmed_word not in corpus_words:
-                        corpus_words[stemmed_word] = 1
+                    if stemmed_word not in self.corpus_words:
+                        self.corpus_words[stemmed_word] = 1
                     else:
-                        corpus_words[stemmed_word] += 1
+                        self.corpus_words[stemmed_word] += 1
 
                     # add the word to our words in class list
-                    class_words[data['class']].extend([stemmed_word])
+                    self.class_words[data['class']].extend([stemmed_word])
 
         # we now have each stemmed word and the number of occurances of the word in our training corpus (the word's commonality)
-        print ("Corpus words and counts: %s \n" % corpus_words)
-        # also we have all words in each class
-        print ("Class words: %s" % class_words)
-        print("\n")
+##        print ("Corpus words and counts: %s \n" % corpus_words)
+##        # also we have all words in each class
+##        print ("Class words: %s" % class_words)
+##        print("\n")
 
 # calculate a score for a given class taking into account word commonality
-def calculate_class_score(sentence, class_name, show_details=True):
-    score = 0
-    # tokenize each word in our new sentence
-    for word in nltk.word_tokenize(sentence):
-        # check to see if the stem of the word is in any of our classes
-        if stemmer.stem(word.lower()) in class_words[class_name]:
-            # treat each word with relative weight
-            score += (1 / corpus_words[stemmer.stem(word.lower())])
+    def calculateClassScore(self, sentence, class_name, show_details=True):
+        score = 0
+        # tokenize each word in our new sentence
+        for word in nltk.word_tokenize(sentence):
+            # check to see if the stem of the word is in any of our classes
+            if self.stemmer.stem(word.lower()) in self.class_words[class_name]:
+                # treat each word with relative weight
+                score += (1 / self.corpus_words[self.stemmer.stem(word.lower())])
 
-            if show_details:
-                print ("   match: %s (%s)" % (stemmer.stem(word.lower()), 1 / corpus_words[stemmer.stem(word.lower())]))
-    return score
+                if show_details:
+                    print ("   match: %s (%s)" % (self.stemmer.stem(word.lower()), 1 / self.corpus_words[self.stemmer.stem(word.lower())]))
+        return score
 
-# # return the class with highest score for sentence
-# def classify(sentence):
-#     high_class = None
-#     high_score = 0
-#     # loop through our classes
-#     for c in class_words.keys():
-#         # calculate score of sentence for each class
-#         score = calculate_class_score(sentence, c, show_details=False)
-#         # keep track of highest score
-#         if score > high_score:
-#             high_class = c
-#             high_score = score
-
-#     return high_class, high_score
-
-
+    def test(self, sentence):
+        
+        scores = []
+        # loop through classes
+        for c in self.class_words.keys():
+            # calculate score of sentence for each class
+            score = self.calculateClassScore(sentence, c, show_details=False)
+            scores.append(score)
+            
+        return scores
 
 classifier = Classifier()
 classifier.train()
-
+print(classifier.test("this is the end of the world"))
 
 
 
