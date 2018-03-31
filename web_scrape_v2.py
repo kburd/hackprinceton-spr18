e0b59142b822397c0ed5b22ce5650016c0a5de62
Nick Rife <nrife@udel.edu>
2018-03-31 02:13:18 -0400

    tried implementing while loop FAILED

--- web_scrape_v2.py
+++ web_scrape_v2.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
-import urllib.request
+import urllib3 as lib
 import sys
 
 
@@ -20,15 +20,28 @@ def text_from_html(body):
 
 
 def parse_and_save_article(link='https://www.cnn.com/2018/03/30/politics/scott-pruitt-epa-white-house/index.html'):
-	html = urllib.request.urlopen(link).read()
+	http = lib.PoolManager()
+	response = http.request('GET', link)
+	html = response.data
+	# print(text_from_html(html))
+	# html = urllib3.request.urlopen(link).read()
 
-	'''
 	# JUNK TO USE LATER...
-	name_idx = link.index('www')+3
+	name_idx = link.index('www')+4
 	outlet_name = link[name_idx:name_idx+3]
-	'''
 
-	file = open('./data/cnn1.txt','a')
+	# # iterate to find the next available number
+	# i = 1
+	# while True:
+	# 	try:
+	# 		open('Articles/' + outlet_name + str(i) + '.txt', 'a')
+	# 		i+=1
+	# 		pass
+	# 	except:
+	# 		break
+
+	# file = open('Articles/' + outlet_name + str(i) + '.txt', 'w')
+	file = open('Articles/cnn1.txt','w')
 	file.write(text_from_html(html))
 	file.close()
 
